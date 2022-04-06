0d048b1a315ec57c2262cc7b5a09f7aa114e1ddb
Jim Spisto <jspisto@worcester.edu>
2022-04-06 14:41:28 -0400

    fix: changed config.ts so app runs in browser

--- client/helpers/config.ts
+++ client/helpers/config.ts
@@ -2,13 +2,14 @@ interface IConfig {
   BASE_SERVER_URL: string
   WEBSOCKET_SERVER_URL: string 
 }
-
-export const config: IConfig = {
-  BASE_SERVER_URL: 'http://192.168.1.201:3000',
-  WEBSOCKET_SERVER_URL: 'ws://192.168.1.201:3000'  
-}
-
+//For mobile
 // export const config: IConfig = {
-//   BASE_SERVER_URL: 'http://localhost:3000',
-//   WEBSOCKET_SERVER_URL: 'ws://localhost:3000'
+//   BASE_SERVER_URL: 'http://192.168.1.201:3000',
+//   WEBSOCKET_SERVER_URL: 'ws://192.168.1.201:3000'  
 // }
+
+//For in browser
+export const config: IConfig = {
+  BASE_SERVER_URL: 'http://localhost:3000',
+  WEBSOCKET_SERVER_URL: 'ws://localhost:3000'
+}
