b6a906b09996f7d3b75bcdfcad5b27dd29f02815
Intelliapps <41976910+intelliapps-io@users.noreply.github.com>
2022-04-19 20:37:46 -0400

    fix: apk build

--- client/helpers/config.ts
+++ client/helpers/config.ts
@@ -2,16 +2,6 @@ interface IConfig {
   BASE_SERVER_URL: string
   WEBSOCKET_SERVER_URL: string 
 }
-//For mobile
-// export const config: IConfig = {
-//   BASE_SERVER_URL: 'http://10.71.55.254:3000',
-//   WEBSOCKET_SERVER_URL: 'ws://10.71.55.254:3000'  
-// }
-
-// export const config: IConfig = {
-//   BASE_SERVER_URL: 'http://192.168.1.201:3000',
-//   WEBSOCKET_SERVER_URL: 'ws://192.168.1.201:3000'  
-// }
 
 const domain = 'https://tiktactoe-live.herokuapp.com'
 
@@ -21,12 +11,12 @@ let config: IConfig = {
   WEBSOCKET_SERVER_URL: domain
 }
 
-if (window.location.href.indexOf('localhost') > -1) {
-  config = {
-    BASE_SERVER_URL: 'http://localhost:3000',
-    WEBSOCKET_SERVER_URL: 'http://localhost:3000'
-  }
-}
+//For mobile
+// export const config: IConfig = {
+//   BASE_SERVER_URL: 'http://10.71.55.254:3000',
+//   WEBSOCKET_SERVER_URL: 'ws://10.71.55.254:3000'  
+// }
+
 
 export { config }
 
